2f788f24f6342726e6d4c9e1b1ed583054ed4504
quinn-5 <quinn-5@comcast.net>
2021-04-10 02:01:00 -0400

    fixed ir read function

--- ReadIR/ReadIR.ino
+++ ReadIR/ReadIR.ino
@@ -2,81 +2,102 @@
 #define IRPin       1 << 2  // Mask to read pin 2
 #define record      1 << 3  // Mask to read pin 3
 #define play        4       // play button pin
+#define ledPin      5       // IR LED pin
 
 #define RESOLUTION  20
 #define MAX_BYTE    255
+#define CMD_SIZE    64
 
 // 2d array to save IR input
-unsigned int cmd[64][2];
+unsigned int cmd[CMD_SIZE][2];
 // 3d array to store multiple commands
-byte storage[5][64][2];
+unsigned int storage[2][CMD_SIZE][2];
 
 bool hasrun = false;
 unsigned int highpulse = 0, lowpulse = 0;   // pulse length 
 
 void setup() {
-  Serial.begin(9600);
-  Serial.println("Ready to decode IR!");
+    Serial.begin(9600);
+    Serial.println("Ready to decode IR!");
 }
 
 void loop() {
-    if (ReadPins & PIND) {
+    if (ReadPins & record) {
         readIR();
-    }
-    if (digitalRead(play)) {
+    } else if (digitalRead(play)) {
         replay();
     }
 }
 
 
 void readIR() {
-    unsigned int index = 0;
+    while (true) {
+        unsigned int index = 0;
     
 
-    while (ReadPins & record) {
-        highpulse = 0, lowpulse = 0;
-        hasrun=true;
-        
-        while (ReadPins & IRPin) {
-            highpulse++;
-            delayMicroseconds(RESOLUTION);
+        while (ReadPins & record) {
+            highpulse = 0, lowpulse = 0;
+            hasrun=true;
 
-            if (!(ReadPins & record)) {
-                return;
+            while (ReadPins & IRPin) {
+                highpulse++;
+                delayMicroseconds(RESOLUTION);
+
+                if (!(ReadPins & record)) {
+                    break;
+                }
             }
-        }
-        cmd[index][0] = highpulse;
-        
-        while (!(ReadPins & IRPin)) {
-            lowpulse++;
-            delayMicroseconds(RESOLUTION);
+            cmd[index][0] = highpulse;
 
-            if (!(ReadPins & record)) {
-                return;
-            }
-        }
-        cmd[index][1] = lowpulse;
-        index++;
+            while (!(ReadPins & IRPin)) {
+                lowpulse++;
+                delayMicroseconds(RESOLUTION);
 
-    }
-    if (hasrun) {
-        for (int i = 0; i < 100; i ++) {
-            if (cmd[i][0] < MAX_BYTE && cmd[i][1] < MAX_BYTE) {
-                storage[1][i][0] = (byte)cmd[i][0];
-                storage[1][i][1] = (byte)cmd[i][1];
-                Serial.print(cmd[i][0]);
+                if (!(ReadPins & record)) {
+                    break;
+                }
+            }   
+            cmd[index][1] = lowpulse;
+            index++;
+        }
+        if (hasrun) {
+            for (int i = 1; i < CMD_SIZE; i ++) {
+                storage[0][i-1][0] = cmd[i][0];
+                storage[0][i-1][1] = cmd[i][1];
+                if (cmd[i][0] == 0) {
+                    break;
+                }            
+                Serial.print(storage[0][i-1][0] * RESOLUTION);
                 Serial.print(", ");
-                Serial.println(cmd[i][1]);
+                Serial.println(storage[0][i-1][1] * RESOLUTION);
+                
+                cmd[i][0] = 0;
+                cmd[i][1] = 0;
             }
-            if (cmd[i][0] == 0) {
-                break;
-            }
+            Serial.println("Done!");
+            return;
         }
-        Serial.println("Done!");
-        hasrun = false;
     }
 }
 
 void replay() {
-    Serial.println("Play button pressed.");
+    long microsecs;
+    //cli();
+    for (int i = 0; i < CMD_SIZE; i++) {
+        microsecs = storage[0][i][1] * RESOLUTION;
+        while (microsecs > 0) {
+            //int a = micros();
+            digitalWrite(ledPin, HIGH);
+            delayMicroseconds(10);
+            digitalWrite(ledPin, LOW);
+            delayMicroseconds(10);
+            //int b = micros();
+            //b -= a;
+            //Serial.println(b);
+            //number of microseconds in 38KHz pulse
+            microsecs -= 26;
+        }
+        delayMicroseconds(storage[0][i][0] * RESOLUTION);
+    }
+    //sei();
 }
